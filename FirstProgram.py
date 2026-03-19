6e7bf5fec3e87f8a4840dfe592293b1b703acf1e
Ethan Pendergraft <ethanpendergraft@gmail.com>
2026-03-19 11:26:22 -0500

    QMainWindow, setSize, and simple button

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,11 +1,22 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-
 import sys
 
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("First Program")
+        button = QPushButton("Click me")
+
+        #self.setFixedSize(QSize(400, 300))
+        self.setMinimumSize(QSize(100, 100))
+        self.setMaximumSize(QSize(500, 500))
+
+        self.setCentralWidget(button)
+
 app = QApplication(sys.argv)
-
-window = QWidget()
+window = MainWindow()
 window.show()
-
-
 app.exec()
